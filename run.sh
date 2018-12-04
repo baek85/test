cae5e31ae21516d7d2c95a9d1781f781ac96d906
Sunghyun Baek <37875752+baek85@users.noreply.github.com>
2018-12-05 00:22:38 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 ## Libraries
 
-	conda install torchvision -c soumith
-	conda install torchvision
+	conda install pytorch=0.4.0 cuda80 -c pytorch
 	conda install imageio
 	conda install matplolib
 	conda install --channel https://conda.anaconda.org/menpo opencv3
