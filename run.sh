c048d58a4ae299e2e950e5fce6b543f27d661b65
Sunghyun Baek <37875752+baek85@users.noreply.github.com>
2018-12-05 00:24:21 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 ## Libraries
 
 	conda install pytorch=0.4.0 cuda80 -c pytorch
-	conda install imageio
+	
+	conda install -c conda-forge matplotlib 
 	conda install matplolib
+	conda install imageio
 	conda install --channel https://conda.anaconda.org/menpo opencv3
 	conda install -c auto easydict	
